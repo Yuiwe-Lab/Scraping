570e54cab9dc53f778ca8f32b99d620ec2b37b27
Mick G <38596586+picklethis@users.noreply.github.com>
2020-07-30 01:13:09 -0400

    Update rsssorter.py
    Updated with additional word_base for feeds

--- rsssorter.py
+++ rsssorter.py
@@ -15,28 +15,26 @@ def create_master(feed_link):
 	for entry in feedpage.entries:
 		link = entry.get("link")
 		if "slashdot" in link[:35]:						#slasdot trim params
-			link_junk = link.rfind("/")
-			link = link[:link_junk]
+			link = link[:link.rfind("/")]
 			to_trim = entry.get("summary")
-			junk = to_trim.find("<p>")
-			summ = to_trim[:junk]
-			summ = wordize(summ)
+			summ = wordize(to_trim[:to_trim.find("<p>")])
 			sift_summ(summ,link,master_list)
 		elif "sciencemag" in link[:30]:					#scienceman trim params
-			link_junk = link.find("?")
-			link = link[:link_junk]
+			link = link[:link.find("?")]
 			to_trim = entry.get("summary")
-			summ = to_trim[3:-4]
-			summ = wordize(summ)
+			summ = wordize(to_trim[3:-4])
 			sift_summ(summ,link,master_list)
 		elif "popsci" in link[:25]:						#popsci trim params
-			link_junk = link.find("?")
-			link = link[:link_junk]
-			to_trim = entry.get("summary")
+			link = link[:link.find("?")]
+			to_trim = entry.get("description")
 			summ = wordize(to_trim)
 			sift_summ(summ,link,master_list)
 		elif "motherjones" in link[:30]:				#mother jones trim params
 			summ = wordize(entry.get("summary")) 		#lucky i guess
+			sift_summ(summ,link,master_list)
+		elif "randomcrit" in link[:35]:
+			link = link[:46] 
+			summ = wordize(entry.get("summary"))
 			sift_summ(summ,link,master_list)
 		else:
 			print(f"could not find a match for {link}")
@@ -47,22 +45,24 @@ def create_master(feed_link):
 rss_urls = (r"http://science.sciencemag.org/rss/express.xml",			#list of RSS URLS to check
 			r"http://rss.slashdot.org/Slashdot/slashdotMain",
 			r"https://www.popsci.com/arcio/rss/",
-			r"https://www.motherjones.com/feed/"
+			r"https://www.motherjones.com/feed/",
+			r"https://randomcriticalanalysis.com/feed/"
 			)	
 
 
 def wordize(ts):						#once a large string with text is achieved, turn it into a list of words
-	puncts = '''!()[]{};:"\,<>./?@#$%^&*_~'''
+	puncts = '''!()[]{};:"\,<>./?@#=$%^&*_~'''
 	for char in ts:					#remove punctuation
 		if char in puncts:
 			ts = ts.replace(char,"")
 	li = list(ts.split(" "))
 	return li
 	
-def sift_summ(summary,link,list):						#sifting parameters
+def sift_summ(summary,link,li):						#sifting parameters
 	ship_words = []
 	ation_words = []
 	hyphen_words = []
+	neering_words = []
 	counter = 0
 	
 	for word in summary:
@@ -75,22 +75,26 @@ def sift_summ(summary,link,list):						#sifting parameters
 				ation_words.append(word)
 				counter += 1
 		if "-" in word:
-			if word not in hyphen_words and word not in hyphen_except:
+			if word not in hyphen_words and word not in hyphen_except and word[:4] != "href":
 				hyphen_words.append(word)
+				counter += 1
+		if word[-7:] == 'neering':
+			if word not in neering_words and word not in neering_except:
+				neering_words.append(word)
 				counter += 1
 		else:
 			pass
-
 	if counter > 0:
-		tuple = (link,ship_words,ation_words,hyphen_words)
-		list.append(tuple) 
+		tuple = (link,ship_words,ation_words,hyphen_words,neering_words)
+		li.append(tuple) 
 	elif counter == 0:
 		pass
 
 hyphen_except =["COVID-19","--","SARS-CoV-2","Covid-19","-"]
 ation_except = []
 ship_except = []
-		
+neering_except = []	
+	
 def db_connect(db_file):
 	conn = None
 	try:
@@ -140,20 +144,30 @@ hyphen_words = '''CREATE TABLE IF NOT EXISTS hyphen_words (
 								FOREIGN KEY (link_id)
 								REFERENCES links(link_id)
 								); '''
+								
+neering_words = '''CREATE TABLE IF NOT EXISTS neering_words (
+								id integer PRIMARY KEY,
+								neering_word text NOT NULL,
+								link_id INTEGER,
+								CONSTRAINT fk_links
+								FOREIGN KEY (link_id)
+								REFERENCES links(link_id)
+								); '''
 		
 
 
 
 with db_connect('wordsort.db') as conn:									#Database Connection creates/checks in CWD
-	make_tables(conn,links_table,ship_words,ation_words,hyphen_words)	#Creates tables for word management and foreign key
+	make_tables(conn,links_table,ship_words,ation_words,hyphen_words,neering_words)	#Creates tables for word management and foreign key
 	for href in rss_urls:												#RSS Link loop start
 		feed_analysis = create_master(href)									#make list of tuples [link,ship_words,...]
 		for entry in feed_analysis:												#Entry loop start
 			cur = conn.cursor()
 			cur.execute("SELECT linkurl FROM links WHERE linkurl=?",(entry[0],))	#check if link is in database
 			result = cur.fetchall()													
-			if len(result) == 0:							#if no rows selected, its not in there
-				lnk_id = None															
+			if len(result) == 0:
+				lnk_id = None
+			#if no rows selected, its not in there
 				cur = conn.cursor()
 				print(entry)
 				cur.execute("INSERT INTO links (linkurl) VALUES (?)",(entry[0],))	#put the link in the database
@@ -174,6 +188,11 @@ with db_connect('wordsort.db') as conn:									#Database Connection creates/che
 					for word in entry[3]:
 						cur = conn.cursor()
 						cur.execute("INSERT INTO hyphen_words(hyphen_word,link_id) VALUES (?,?)",(word,lnk_id))
+						conn.commit()
+				if len(entry[4]) > 0:
+					for word in entry[4]:
+						cur = conn.cursor()
+						cur.execute("INSERT INTO neering_words(neering_word,link_id) VALUES (?,?)",(word,lnk_id))
 						conn.commit()
 	print("I'm Done...")
 print("connection Closed")
