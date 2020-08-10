9687b5fa85b0bb647a07175a8e3fb9112b51a720
Mick G <38596586+picklethis@users.noreply.github.com>
2020-08-10 02:31:48 -0400

    Update feeder.py
    Updated with TED examples

--- RSS Scraper/feeder.py
+++ RSS Scraper/feeder.py
@@ -10,6 +10,7 @@ import sqlite3
 from sqlite3 import Error
 import requests
 
+#rss.app currently Broken
 rss_urls = [r"http://rss.slashdot.org/Slashdot/slashdotMain",	#Slashdot
 			r"https://www.popsci.com/arcio/rss/",				#Popsci
 			r"https://www.motherjones.com/feed/",				#Mother Jones
@@ -19,7 +20,11 @@ rss_urls = [r"http://rss.slashdot.org/Slashdot/slashdotMain",	#Slashdot
 			#r"https://rss.app/feeds/fHPzIIzHJVqtIjr3.xml", 	 	#Entrpreneur
 			#r"https://rss.app/feeds/1Pqsw7njparMZi7B.xml",  	#wired
 			#r"https://rss.app/feeds/feANofWEfawQKkQr.xml",		#Fast Company
-			r"https://plato.stanford.edu/rss/sep.xml"		    #Philosophy Dictionary
+			r"https://plato.stanford.edu/rss/sep.xml",		    #Philosophy Encyclopedia
+			r"https://www.ted.com/talks/ibram_x_kendi_the_difference_between_being_not_racist_and_antiracist",
+			r"https://www.ted.com/talks/bill_gates_how_the_pandemic_will_shape_the_near_future/",
+			r"https://www.ted.com/talks/matt_walker_how_caffeine_and_alcohol_affect_your_sleep",
+			r"https://www.ted.com/talks/chloe_valdary_how_to_use_love_to_repair_social_inequality"
 		  ]
 
 
